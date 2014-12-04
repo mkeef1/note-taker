58634cc7852501698a6c2ba92bca39e207d79f41
Matt Keef <mkeef1@gmail.com>
2014-12-04 09:21:33 -0600

    id attached to note

--- server/routes/definitions/notes/post_notes.js
+++ server/routes/definitions/notes/post_notes.js
@@ -5,7 +5,8 @@ var Note = require('../../../models/note');
 module.exports = {
   description: 'post a new note',
   handler: function(req, rep){
-    Note.addNote(req.payload, req.auth.credentials.id, function(err, note){
+    req.payload.userId = req.auth.credentials.id;
+    Note.addNote(req.payload, function(err, note){
       rep();
     });
   }
